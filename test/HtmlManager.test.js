577cb17429565a5ebe929df515dd11c7a1fc40a4
Anthony obeta <choobey21@gmail.com>
2017-06-20 22:16:22 -0700

    HTML manager code was migrated to pagescraper.js

--- test/HtmlManager.test.js
+++ test/HtmlManager.test.js
@@ -1,5 +1,3 @@
 const HManager = require("../src/HtmlManager");
 
-test("throws an error if no url is provided", () => {
-  expect(HManager()).rejects.toBe("there was an error scraping the page");
-});
+test("throws an error if there is no HTML file to parse", () => {});
