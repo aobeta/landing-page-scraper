b9ee9fd7e65946a0b84128a5bb42db7ab4176449
Anthony obeta <choobey21@gmail.com>
2017-07-04 17:57:59 -0700

    written tests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const HtmlManager = require("./src/HtmlManager");
 const scraper = require("./src/pagescraper");
+const hManager = require("./src/HtmlManager");
 
 // once we are finished this is how we make this library available to the outside world ( strawhouse devs )
 module.exports = function(url, options) {
--- src/HtmlManager.js
+++ src/HtmlManager.js
@@ -1,6 +1,28 @@
-const _ = require("lodash");
 const scrape = require("website-scraper");
+const dirSearch = require("directory-search");
+const fs = require("fs");
+const cheerio = require("cheerio");
+const _ = require("lodash");
 
-function HtmlManager(url, options) {}
+function HtmlManager() {
+  return new Promise((resolve, reject) => {
+    dirSearch("dist", ".html", function(err, files) {
+      if (files.length !== 1)
+        reject("There is more or less than one Html file");
+      fs.readFile(files[0], "utf8", (error, data) => {
+        resolve(data);
+      });
+    });
+  }).then(removeTrackingScripts);
+}
+
+function removeTrackingScripts(html) {
+  const $ = cheerio.load(html);
+  console.log("script tags :", $("script"));
+  $("script").remove();
+  //   console.log($.html());
+}
+
+function cleanCss() {}
 
 module.exports = HtmlManager;
