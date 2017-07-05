e2111aa7e3efb7f60bce74b1bd12a242bb44effd
Anthony obeta <choobey21@gmail.com>
2017-07-05 01:00:33 -0700

    duplicate import

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const HtmlManager = require("./src/HtmlManager");
 const scraper = require("./src/pagescraper");
-const hManager = require("./src/HtmlManager");
 
 // once we are finished this is how we make this library available to the outside world ( strawhouse devs )
 module.exports = function(url, options) {
